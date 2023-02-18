b9d0059a9c0b325f6440eeffb2abe313145448a9
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 12:02:33 +0700

    Data Type: Character

---
+++ src/main/kotlin/basic/data/type/character/Character.kt
@@ -0,0 +1,13 @@
+package basic.data.type.character
+
+fun main() {
+
+    // Char can only hold one value (character)
+    // Char value is surrounded by single quotes
+    var alpha: Char = 'a'
+    var numeric: Char = '1'
+
+    println("Alpha character : " + alpha)
+    println("Numeric character : " + numeric)
+
+}
