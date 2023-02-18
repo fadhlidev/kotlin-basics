41f7e7bdef587aa63dbe8881ccac9c1ba391e9a5
Fadhli Syaifullah <me@fadhli.dev>
2023-02-19 06:43:00 +0700

    Variable: Constant

---
+++ src/main/kotlin/basic/variable/Contant.kt
@@ -0,0 +1,12 @@
+package basic.variable
+
+// Constants are immutable data that can be accessed for global usage
+// Use the const val keyword to declare a constant
+// Usually constant variable use uppercase name
+const val PROJECT_NAME = "Kotlin Basics"
+
+fun main() {
+
+    println(PROJECT_NAME)
+
+}
