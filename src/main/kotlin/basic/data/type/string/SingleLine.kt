c9779a499cda8218b9d17356bf30b6b13464571e
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 13:18:19 +0700

    Data Type: Single Line String

---
+++ src/main/kotlin/basic/data/type/string/SingleLine.kt
@@ -0,0 +1,9 @@
+package basic.data.type.string
+
+fun main() {
+
+    // String (text) values are surrounded by double quotes
+    var name: String = "Fadhli Syaifullah"
+    println("Name : " + name)
+
+}
