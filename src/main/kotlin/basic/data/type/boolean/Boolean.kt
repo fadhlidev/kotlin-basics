5f15f4820a5ac49b8cef50b99ba59bc6e8a0efb1
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 13:12:53 +0700

    Data Type: Boolean

---
+++ src/main/kotlin/basic/data/type/boolean/Boolean.kt
@@ -0,0 +1,12 @@
+package basic.data.type.boolean
+
+fun main() {
+
+    // Boolean values are only either true or false
+    var truthy: Boolean = true
+    println("Truthy value : " + truthy)
+
+    var falsy: Boolean = false
+    println("Falsy value : " + falsy)
+
+}
