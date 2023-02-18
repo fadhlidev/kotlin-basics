acb245d829e6847914f99cedfa91075cb8d12a6b
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 11:34:47 +0700

    Data Type: Floating Point Numbers

---
+++ src/main/kotlin/basic/data/type/number/FloatingPoint.kt
@@ -0,0 +1,14 @@
+package basic.data.type.number
+
+fun main() {
+
+    // Float digits: 6-7
+    // Use F to mark value as a floating point
+    var float: Float = 27.5F
+    println("float value : " + float)
+
+    // Double digits: 15-16
+    var double: Double = 99_999_999.999
+    println("double value : " + double)
+
+}
