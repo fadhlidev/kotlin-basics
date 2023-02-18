b7a2796e3557afe7e74ea37a12247532b980a356
Fadhli Syaifullah <me@fadhli.dev>
2023-02-19 06:28:15 +0700

    Variable: Immutable

---
+++ src/main/kotlin/basic/variable/Immutable.kt
@@ -0,0 +1,9 @@
+package basic.variable
+
+fun main() {
+
+    // Use the val keyword to declare an immutable variable
+    // Immutable value must be initialized, and it can't be reassigned
+    val fullName = "Fadhli Syaifullah"
+
+}
