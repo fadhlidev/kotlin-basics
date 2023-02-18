a149f557cf2ae0aab8fe7f8801d04229cda82a38
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 13:33:09 +0700

    Data Type: String Concatenation

---
+++ src/main/kotlin/basic/data/type/string/Concatenation.kt
@@ -0,0 +1,15 @@
+package basic.data.type.string
+
+fun main() {
+
+    var firstName: String = "Fadhli"
+    println("first name : " + firstName)
+
+    var lastName: String = "Syaifullah"
+    println("last name : " + lastName)
+
+    // We can concat two or more strings together using + operator
+    var fullName: String = firstName + " " + lastName
+    println("full name : " + fullName)
+
+}
