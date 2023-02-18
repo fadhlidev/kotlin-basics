c5ab7a651d35b942c1f0c7db8e949b56ffb53f3f
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 12:59:45 +0700

    Data Type: Literal Numbers

---
+++ src/main/kotlin/basic/data/type/number/Literal.kt
@@ -0,0 +1,14 @@
+package basic.data.type.number
+
+fun main() {
+
+    var decimal: Int = 100
+    println("decimal value : " + decimal)
+
+    var hexadecimal: Int = 0x64
+    println("hexadecimal value : " + hexadecimal)
+
+    var binary: Int = 0b1100100
+    println("binary value : " + binary)
+
+}
