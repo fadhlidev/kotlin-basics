62e347525be396a7e451b5b5a6111fb44e11c6b1
Fadhli Syaifullah <me@fadhli.dev>
2023-02-18 13:23:55 +0700

    Data Type: Multi Lines String

---
+++ src/main/kotlin/basic/data/type/string/MultiLines.kt
@@ -0,0 +1,13 @@
+package basic.data.type.string
+
+fun main() {
+
+    // For multilines string, text values are enclosed with """
+    var address: String = """
+        Some Street No. 0
+        Some Region, Some Country
+    """
+
+    println(address)
+
+}
