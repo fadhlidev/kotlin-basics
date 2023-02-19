fbb22a0ebf975aee663a17fa200c74d7e002af47
Fadhli Syaifullah <me@fadhli.dev>
2023-02-19 07:28:52 +0700

    Data Type: Nullable Array (Fix typo)

--- src/main/kotlin/basic/data/type/array/NullableArray.kt
+++ src/main/kotlin/basic/data/type/array/NullableArray.kt
@@ -2,7 +2,7 @@ package basic.data.type.array
 
 fun main() {
 
-    // By default, we can store null value inside an array
+    // By default, we can't store null value inside an array
     // But, we can tell kotlin that an array my store null values
     // To do this, use ? after the Array type declaration
     var names: Array<String?> = arrayOfNulls(3)
