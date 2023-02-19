3c2ba15a1d7a94ca81d0a6afc014496796e082f7
Fadhli Syaifullah <me@fadhli.dev>
2023-02-19 07:04:38 +0700

    Data Type: Array

---
+++ src/main/kotlin/basic/data/type/array/Array.kt
@@ -0,0 +1,11 @@
+package basic.data.type.array
+
+fun main() {
+
+    // Array is a data type that contains a collection of data of the same type
+    // Array data type is represented by the Array keyword
+    // ex: Array<String> means array of strings
+    var names: Array<String> = arrayOf("Fadhli", "Syaifullah")
+    var numbers: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+
+}
