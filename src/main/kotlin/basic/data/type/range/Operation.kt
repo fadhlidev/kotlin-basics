8c4efd78347b4fec9d39abbfb4c37c041885ee2e
Fadhli Syaifullah <me@fadhli.dev>
2023-02-19 07:26:39 +0700

    Data Type: Range Operation

---
+++ src/main/kotlin/basic/data/type/range/Operation.kt
@@ -0,0 +1,14 @@
+package basic.data.type.range
+
+fun main() {
+
+    var grades = 0..100
+
+    // To access and perform operations with a range, use the available properties and built-in methods
+    // These are some examples:
+    println(grades.count())
+    println(grades.first)
+    println(grades.last)
+    println(grades.step)
+
+}
